3739e8999341c18bdcf0a512bc0b2ff552078f88
Quentin Laplanche <quentin.laplanche@outlook.fr>
2016-10-21 10:55:35 +0200

    Setting up Xcode
    Signed-off-by: Quentin Laplanche <quentin.laplanche@outlook.fr>

---
+++ Geoloc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Geoloc
+//
+//  Created by Quentin Laplanche on 21/10/2016.
+//  Copyright © 2016 projet-geoloc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
